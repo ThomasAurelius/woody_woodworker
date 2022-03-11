e880beab41c5c67c6d14dde4f53cb463c0ce34b4
Thomas Aurelius <45251987+ThomasAurelius@users.noreply.github.com>
2022-03-11 16:54:07 -0600

    updated responsiveness

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function fixNav() {
       nav.classList.remove('active')
    }
 }
+
+// const navToggle = document.querySelector('.nav-toggle');
+// const navLinks = document.querySelectorAll('.nav_link')
+
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('nav-open');
+//     })
+// })
